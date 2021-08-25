69d2df7cedda64a26c1bdeaad10d6bedcaa26286
Nikolay Yakimov <root@livid.pp.ru>
2021-08-25 23:16:12 +0300

    Tweaks

--- keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
+++ keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
@@ -7,19 +7,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,  KC_Q, KC_W,   KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
         KC_TAB,  RGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
         KC_LGUI, KC_Z, KC_X,   KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
-        KC_CAPS, KC_SLCK, 84, MO(1), LSFT_T(KC_BSPC), LT(4,KC_ENT), RSFT_T(KC_SPC), MO(2), KC_UP, KC_DOWN, TG(5)
+        KC_SLCK, KC_CAPS, KC_GRV, MO(LOWER), LSFT_T(KC_BSPC), LT(NAV,KC_ENT), RSFT_T(KC_SPC), MO(UPPER), KC_LBRC, KC_RBRC, TG(NUMS)
         ),
 	[LOWER] = LAYOUT_planck_mit(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,       KC_ASTR,       KC_LPRN, KC_RPRN, KC_BSPC,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,       KC_PLUS,       KC_LCBR, KC_RCBR, KC_PIPE,
         KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  LSFT(KC_NUHS), LSFT(KC_NUBS), KC_HOME, KC_END,  KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3),         KC_TRNS,       KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(CTRL),      KC_TRNS,       KC_TRNS, KC_TRNS
         ),
 	[UPPER] = LAYOUT_planck_mit(
-        KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3, KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_TRNS, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_GRV,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_DEL,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_TRNS, KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, MO(CTRL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         ),
 	[CTRL] = LAYOUT_planck_mit(
         KC_TRNS, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
@@ -90,7 +90,7 @@ void rgb_matrix_indicators_user(void) {
     //capslock leds
 
     if (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) {
-        SETC(3,0, RGB_ORANGE);
+        SETC(3,1, RGB_ORANGE);
     }
 
 }
---
+++ keyboards/bm40hsrgb/update-json.sh
@@ -0,0 +1,1 @@
+qmk c2json -km lierdakil -kb bm40hsrgb keymaps/lierdakil/keymap.c --no-cpp | jq '.' > keymaps/lierdakil/kb.json
