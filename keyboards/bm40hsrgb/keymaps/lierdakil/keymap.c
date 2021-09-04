e7ed4e43db042762e8a4b7c4fcaca4ec12be610d
Nikolay Yakimov <root@livid.pp.ru>
2021-09-04 21:08:53 +0300

    tweak caps pos

--- keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
+++ keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
@@ -18,16 +18,16 @@ enum layers {BASE, LOWER, UPPER, CTRL, NAV, NUMS};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_planck_mit(
-        KC_Q,    KC_W,   KC_E,    KC_R, KC_T, KC_EQL,     KC_MINS, KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        MC_A,    MC_S,   MC_D,    MC_F, KC_G, KC_TAB,     KC_QUOT, KC_H, MC_J, MC_K,    MC_L,   MC_SCLN,
-        KC_Z,    KC_X,   KC_C,    KC_V, KC_B, KC_LGUI,    KC_RCTL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-        KC_CAPS, KC_GRV, KC_SLCK, LWR,  BSP,  ENT                , SPC,  UPR,  TG(NUMS),KC_LBRC,KC_RBRC
+        KC_Q,    KC_W,    KC_E,    KC_R, KC_T, KC_EQL,     KC_MINS, KC_Y, KC_U, KC_I,    KC_O,   KC_P,
+        MC_A,    MC_S,    MC_D,    MC_F, KC_G, KC_TAB,     KC_QUOT, KC_H, MC_J, MC_K,    MC_L,   MC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V, KC_B, KC_LGUI,    KC_RCTL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+        KC_GRV,  KC_BSLS, KC_CAPS, LWR,  BSP,  ENT                , SPC,  UPR,  TG(NUMS),KC_LBRC,KC_RBRC
         ),
 	[LOWER] = LAYOUT_planck_mit(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD, KC_BSPC, KC_CIRC, KC_AMPR,       KC_ASTR,       KC_LPRN, KC_RPRN,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_DEL,  KC_PIPE, KC_F6,   KC_UNDS,       KC_PLUS,       KC_LCBR, KC_RCBR,
         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_TRNS, KC_TRNS, KC_F12,  LSFT(KC_NUHS), LSFT(KC_NUBS), KC_HOME, KC_END,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(CTRL),      KC_TRNS,       KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_SLCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(CTRL),      KC_TRNS,       KC_TRNS, KC_TRNS
         ),
 	[UPPER] = LAYOUT_planck_mit(
         KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
@@ -101,13 +101,17 @@ void rgb_matrix_indicators_user(void) {
         }
     }
 
-    //capslock leds
-
-    if (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) {
-        SETC(3,0, RGB_ORANGE);
-    }
-    if (host_keyboard_leds() & (1<<USB_LED_SCROLL_LOCK)) {
+    // status leds
+    switch (host_keyboard_leds() & ((1<<USB_LED_CAPS_LOCK) | (1<<USB_LED_SCROLL_LOCK))) {
+        case 0b10:
         SETC(3,2, RGB_ORANGE);
+        break;
+        case 0b100:
+        SETC(3,2, RGB_GOLDENROD);
+        break;
+        case 0b110:
+        SETC(3,2, RGB_GREEN);
+        break;
     }
 
 }
