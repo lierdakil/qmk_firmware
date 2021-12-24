664cecb630680c79de25f3f982dee4714b2849d9
Nikolay Yakimov <root@livid.pp.ru>
2021-12-24 10:49:00 +0300

    tweak bsp/del

--- keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
+++ keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
@@ -13,7 +13,7 @@ enum layers {BASE, LOWER, UPPER, CTRL, NAV, NUMS};
 #define LWR LT(LOWER,KC_BSPC)
 #define UPR LT(UPPER,KC_SPC)
 #define ENT LT(NAV,KC_ENT)
-#define BSP LSFT_T(KC_BSPC)
+#define DEL LSFT_T(KC_DEL)
 #define SPC RSFT_T(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R, KC_T, KC_EQL,     KC_MINS, KC_Y, KC_U, KC_I,    KC_O,   KC_P,
         MC_A,    MC_S,    MC_D,    MC_F, KC_G, KC_TAB,     KC_QUOT, KC_H, MC_J, MC_K,    MC_L,   MC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V, KC_B, KC_LGUI,    KC_RCTL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-        KC_GRV,  KC_BSLS, KC_CAPS, LWR,  BSP,  ENT                , SPC,  UPR,  TG(NUMS),KC_LBRC,KC_RBRC
+        KC_GRV,  KC_BSLS, KC_CAPS, LWR,  DEL,  ENT               , SPC,  UPR,  TG(NUMS),KC_LBRC,KC_RBRC
         ),
 	[LOWER] = LAYOUT_planck_mit(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD, KC_BSPC, KC_CIRC, KC_AMPR,       KC_ASTR,       KC_LPRN, KC_RPRN,
