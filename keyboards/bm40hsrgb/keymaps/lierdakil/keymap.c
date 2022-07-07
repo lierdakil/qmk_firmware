1c51f778a32f916459e8cf44b3bda473b4829d51
Nikolay Yakimov <root@livid.pp.ru>
2022-07-07 21:30:05 +0300

    Use simpler KC_TRNS

--- keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
+++ keyboards/bm40hsrgb/keymaps/lierdakil/keymap.c
@@ -26,34 +26,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[LOWER] = LAYOUT_planck_mit(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD, KC_BSPC, KC_CIRC, KC_AMPR,       KC_ASTR,       KC_LPRN, KC_RPRN,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_DEL,  KC_PIPE, KC_F6,   KC_UNDS,       KC_PLUS,       KC_LCBR, KC_RCBR,
-        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_TRNS, KC_TRNS, KC_F12,  LSFT(KC_NUHS), LSFT(KC_NUBS), KC_HOME, KC_END,
-        KC_TRNS, KC_TRNS, KC_SLCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(CTRL),      KC_TRNS,       KC_TRNS, KC_TRNS
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  LSFT(KC_NUHS), LSFT(KC_NUBS), KC_HOME, KC_END,
+        _______, _______, KC_SLCK, _______, _______, _______, _______, MO(CTRL),      _______,       _______, _______
         ),
 	[UPPER] = LAYOUT_planck_mit(
         KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_DEL,  KC_BSLS, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
-        KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_TRNS, KC_TRNS, KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN,
-        KC_TRNS, KC_TRNS, KC_TRNS, MO(CTRL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  _______, _______, KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN,
+        _______, _______, _______, MO(CTRL), _______, _______, _______, _______, _______, _______, _______
         ),
 	[CTRL] = LAYOUT_planck_mit(
-        KC_TRNS, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
 	[NAV] = LAYOUT_planck_mit(
     /*  0        1        2        3        4        5        6        7        8        9        10       11       */
-        KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_UP,   KC_PGDN, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, _______,
+        _______, _______, _______, _______, _______, _______         , _______, _______, _______, _______, _______
         ),
 	[NUMS] = LAYOUT_planck_mit(
     /*  0        1        2        3        4        5        6        7        8        9      10       11       */
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_P7, KC_P8,   KC_P9,   KC_PAST,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PMNS, KC_P4, KC_P5,   KC_P6,   KC_PPLS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NLCK, KC_P1, KC_P2,   KC_P3,   KC_PEQL,
-        KC_TRNS, KC_TRNS, KC_PSCR, KC_PAUS, KC_TRNS, KC_TRNS         , KC_TRNS, KC_P0, KC_TRNS, KC_PDOT, KC_PENT
+        _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_P7, KC_P8,   KC_P9,   KC_PAST,
+        _______, _______, _______, _______, _______, _______, _______, KC_PMNS, KC_P4, KC_P5,   KC_P6,   KC_PPLS,
+        _______, _______, _______, _______, _______, _______, _______, KC_NLCK, KC_P1, KC_P2,   KC_P3,   KC_PEQL,
+        _______, _______, KC_PSCR, KC_PAUS, _______, _______         , _______, KC_P0, _______, KC_PDOT, KC_PENT
         )
 };
 
