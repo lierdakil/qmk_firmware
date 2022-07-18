9a97ebf8ddca5a08e5c1e5786a8465ff9b365050
Nikolay Yakimov <root@livid.pp.ru>
2022-07-18 13:46:49 +0300

    tweak pinky keymap smore

--- keyboards/pinky/3/keymaps/lierdakil/keymap.c
+++ keyboards/pinky/3/keymaps/lierdakil/keymap.c
@@ -25,41 +25,41 @@ enum layers {BASE, LOWER, UPPER, CTR, NAV};
 #define MC_K LSFT_T(KC_K)
 #define MC_L LALT_T(KC_L)
 #define MC_SCLN RGUI_T(KC_SCLN)
-#define LWR LT(LOWER,KC_DEL)
-#define UPR LT(UPPER,KC_SPC)
+#define DEL_LWR LT(LOWER,KC_DEL)
+#define SPC_UPR LT(UPPER,KC_SPC)
+#define SPC_CTL LT(CTR,KC_SPC)
+#define DEL_CTL LT(CTR,KC_DEL)
 #define ENT LT(NAV,KC_ENT)
 #define BSPC LSFT_T(KC_BSPC)
 #define SPC RSFT_T(KC_SPC)
-
-#define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x7_4(
         KC_EQL , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LPRN,    KC_RPRN, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
         KC_TAB , MC_A   , MC_S   , MC_D   , MC_F   , KC_G   , KC_LBRC,    KC_RBRC, KC_H   , MC_J   , MC_K   , MC_L   , MC_SCLN, KC_QUOT,
         KC_GRV , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_CAPS,    ENT    , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS ,
-                                   KC_LEFT, KC_RGHT, BSPC   , LWR    ,    UPR    , SPC    , KC_UP  , KC_DOWN
+                                   KC_LEFT, KC_RGHT, BSPC   , DEL_LWR,    SPC_UPR, SPC    , KC_UP  , KC_DOWN
     ),
     [LOWER] = LAYOUT_split_3x7_4(
         KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_HOME,    KC_END , KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         _______, _______, _______, _______, _______, _______, KC_PGUP,    KC_PGDN, _______, _______, _______, KC_MINS, KC_EQL , _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______,    MO(CTR), _______, _______, _______
+                                   _______, _______, _______, _______,    SPC_CTL, _______, _______, _______
     ),
     [UPPER] = LAYOUT_split_3x7_4(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_HOME,    KC_END , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         _______, _______, _______, _______, _______, _______, KC_PGUP,    KC_PGDN, _______, _______, _______, KC_MINS, KC_EQL , _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, MO(CTR), _______,    _______, _______, _______, _______
+                                   _______, _______, _______, DEL_CTL,    _______, _______, _______, _______
     ),
     [CTR] = LAYOUT_split_3x7_4(
-        _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
+        KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                                    RESET  , _______, _______, _______,    _______, _______, _______, _______
     ),
     [NAV] = LAYOUT_split_3x7_4(
-        _______, _______, _______, _______, _______, _______, _______,    _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______,
+        KC_ESC , _______, _______, _______, _______, _______, _______,    _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, KC_HOME, KC_END,  _______, _______, _______,
                                    _______, _______, _______, _______,    _______, _______, _______, _______
