f8a59307a12ee992a809ba793d589eb81096bf56
Nikolay Yakimov <root@livid.pp.ru>
2022-07-31 01:32:20 +0300

    Add custom modifiers

--- keyboards/pinky/3/keymaps/lierdakil/keymap.c
+++ keyboards/pinky/3/keymaps/lierdakil/keymap.c
@@ -32,12 +32,13 @@ enum layers {BASE, LOWER, UPPER, CTR, NAV};
 #define ENT LT(NAV,KC_ENT)
 #define BSPC LSFT_T(KC_BSPC)
 #define SPC RSFT_T(KC_SPC)
+#define MC_CAPS LT(0, KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x7_4(
         KC_EQL , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LPRN,    KC_RPRN, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
         KC_TAB , MC_A   , MC_S   , MC_D   , MC_F   , KC_G   , KC_LBRC,    KC_RBRC, KC_H   , MC_J   , MC_K   , MC_L   , MC_SCLN, KC_QUOT,
-        KC_GRV , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_CAPS,    ENT    , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS ,
+        KC_GRV , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , MC_CAPS,    ENT    , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS ,
                                    KC_LEFT, KC_RGHT, BSPC   , DEL_LWR,    SPC_UPR, SPC    , KC_UP  , KC_DOWN
     ),
     [LOWER] = LAYOUT_split_3x7_4(
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [UPPER] = LAYOUT_split_3x7_4(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_HOME,    KC_END , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         _______, _______, _______, _______, _______, _______, KC_PGUP,    KC_PGDN, _______, _______, _______, KC_MINS, KC_EQL , _______,
-        _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_LNG3,    _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, DEL_CTL,    _______, _______, _______, _______
     ),
     [CTR] = LAYOUT_split_3x7_4(
@@ -71,3 +72,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                                _______, _______, _______, _______,    _______, _______, _______, _______
     // )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MC_CAPS:
+            if (!record->tap.count) {
+                if (record->event.pressed) {
+                    register_code(KC_INT1); // Intercept hold function to send Ctrl-X
+                } else {
+                    unregister_code(KC_INT1); // Intercept hold function to send Ctrl-X
+                }
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+    }
+    return true;
+}
