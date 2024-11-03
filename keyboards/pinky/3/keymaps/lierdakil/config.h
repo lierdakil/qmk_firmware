8a07071df87881e814fc7fbe7bfebbca62912dfa
Nikolay Yakimov <root@livid.pp.ru>
2024-11-03 23:44:44 +0300

    Update pinky3 to new qmk version

--- keyboards/pinky/3/keymaps/lierdakil/config.h
+++ keyboards/pinky/3/keymaps/lierdakil/config.h
@@ -15,15 +15,3 @@
  */
 
 #pragma once
-
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-#define EE_HANDS
-
-#define USE_SERIAL_PD2
-
-#define IGNORE_MOD_TAP_INTERRUPT
